362d862b3cdc740fea5f15401455bde11bc382b8
SoraSkyy <viorama@gmail.com>
2017-05-21 20:21:54 +0800

    Moved user info viewer into the user controller.

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,23 @@
 import { Router } from 'express';
-import User from '../models/user';
-import Book from '../models/book';
+import { UserController } from '../controllers/user-controller';
 
 const router = Router();
 
 /*
-  Testing function that returns a JSON object
-  containing username, email, and description.
+  Returns a JSON object containing username, email, books, gender and description.
 
-  No checks for authentication yet.
+  No checks for authentication.
 */
 router.get('/:username', async (req, res, next) => {
-  let user;
-  try {
-    user = await User.findOne({ username: req.params.username });
-  } catch (err) {
-    console.log('Error occurred while finding user: ');
-    console.log(err);
-    return res.status(404).json({ success: false, error: 'User not found.' });
+  const user = new UserController({ username: req.params.username });
+
+  let result = await user.getUserInfo();
+  if (!result) {
+    return res.status(404).json({ success: false, error: 'Unable to find user.' });
   }
 
-  if (user == null) {
-    return res.status(404).json({ success: false, error: 'User not found.' });
-  }
-
-  // TODO: Ensure this works.
-  let books;
-  try {
-    books = user.books.populate('books').map(book => ({ title: book.title, author: book.author }));
-  } catch (err) {
-    console.log('Error occurred while mapping books: ');
-    console.log(err);
-    return res.status(404).json({ success: false, error: 'Error: Please try again' });
-  }
-
-  return res.status(200).json({
-    success: true,
-    username: user.username,
-    email: user.email,
-    gender: user.gender,
-    books: await books,
-  });
+  result.success = true;
+  return res.status(200).json(result);
 });
 
 export default router;
