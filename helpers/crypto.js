70840b58b309bc647a6224884ea05a8acf2fcad6
SoraSkyy <viorama@gmail.com>
2017-05-22 23:57:47 +0800

    Login function implemented.

--- helpers/crypto.js
+++ helpers/crypto.js
@@ -3,14 +3,21 @@ import bcrypt from 'bcrypt-nodejs';
 export const generateHash = password => new Promise((resolve, reject) => {
   bcrypt.genSalt(8, (err, salt) => {
     if (err) {
-      throw err;
+      reject(err);
     }
     bcrypt.hash(password, salt, null, (hashError, hash) => {
       if (hashError) {
-        throw hashError;
+        reject(hashError);
       }
       resolve(hash);
     });
   });
 });
 
+
+export const compareHash = (password, hash) => new Promise((resolve, reject) => {
+  bcrypt.compare(password, hash, (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
