10e73b2d620f0c080cf772aabbcfddacf5c8081e
SoraSkyy <viorama@gmail.com>
2017-06-14 22:43:17 +0800

    Added book creation functionality (untested).

--- middleware/assert-admin.js
+++ middleware/assert-admin.js
@@ -1,3 +1,10 @@
+/*
+  This middleware is designed to catch all traffic directed to /admin 
+  and makes sure that only those authorized can have access to the admin panel.
+
+  It assumes that the authenticator middleware has already checked for authentication,
+  so make sure this is only used AFTER authenticator.js
+*/
 const assertAdmin = async (req, res, next) => {
   if (!req.authenticated) return res.render('admin/login');
 
