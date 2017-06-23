2b99aed844d596fd7aae9e32aa0e4fd4b7aa93a6
SoraSkyy <viorama@gmail.com>
2017-06-23 21:32:27 +0800

    Changed CORS to allow credentials

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,12 @@
 const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  const allowedOrigins = ['http://localhost:8080', 'http://storkapp.flu.cc'];
+  const origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 };
 
