eaa0e85309514f8eb091df98ba4633da375405a7
SoraSkyy <viorama@gmail.com>
2017-06-29 21:47:48 +0800

    Changed genres to singular rather than array.

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const BookSchema = Schema({
   owner: { type: Schema.ObjectId, ref: 'User' },
   title: { type: String, required: true },
   author: { type: String, required: true },
-  genre: [{ type: Schema.ObjectId, ref: 'Genre' }],
+  genre: { type: Schema.ObjectId, ref: 'Genre' },
   dateListed: { type: Date, required: true },
   description: { type: String, default: '' },
 });
