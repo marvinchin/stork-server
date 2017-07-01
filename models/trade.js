01fd7b5c24494ab4f06f803e406510eaa8379713
SoraSkyy <viorama@gmail.com>
2017-07-02 01:55:58 +0800

    Mostly completed createTrade api with admin testing, excluding return of trade information which has not been tested yet.

--- models/trade.js
+++ models/trade.js
@@ -5,7 +5,7 @@ const TradeSchema = Schema({
   offerUser: { type: String, required: true },
   listBook: { type: Schema.ObjectId, ref: 'Book', required: true },
   offerBooks: [{ type: Schema.ObjectId, ref: 'Book', required: true }],
-  description: { type: String, default: '', required: true },
+  description: { type: String, default: '' },
   selectedBook: { type: Schema.ObjectId },
   tradeStatus: { type: String, enum: ['P', 'A', 'C', 'OC', 'LC', 'BC'], required: true },
 });
