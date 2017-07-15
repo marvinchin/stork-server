ed7f9b5b991b65e2f57582d1469ceb8625bb9975
SoraSkyy <viorama@gmail.com>
2017-07-15 21:26:11 +0800

    Implemented update trade api for accepting trade.

--- middleware/assert-admin.js
+++ middleware/assert-admin.js
@@ -9,7 +9,8 @@ const assertAdmin = async (req, res, next) => {
   if (!req.authenticated) return res.render('admin/login');
 
   // TODO: Fix this hardcoding of username.
-  if (req.session.auth.username !== 'yj123') return res.status(403).render();
+  if (req.session.auth.username !== 'yj123' &&
+     req.session.auth.username !== 'yj234') return res.status(403).render();
 
   return next();
 };
