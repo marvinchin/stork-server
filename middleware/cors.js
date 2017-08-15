3cec99d47e914565bf08f4ec7a29231dac5d0513
marvinchin <chinkunsong.marvin@gmail.com>
2017-08-15 23:06:46 +0800

    Add alternate origin for CORS

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,19 @@
 const cors = (req, res, next) => {
-  const allowedOrigins = ['http://localhost:8080', 'http://storkapp.flu.cc'];
+  const allowedOrigins = [
+    "http://localhost:8080",
+    "http://storkapp.flu.cc",
+    "http://stork.marvinchin.me"
+  ];
   const origin = req.headers.origin;
   if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 };
 
