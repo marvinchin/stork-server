825a397a4e2d23fb75d219d66dd9bb0db1edd355
marvinchin <chinkunsong.marvin@gmail.com>
2017-08-21 03:27:08 +0800

    Strip data prefix before saving profile picture

--- helpers/filesystem.js
+++ helpers/filesystem.js
@@ -1,15 +1,20 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export const saveProfilePicture = (username, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(path.resolve(__dirname, `../public/profile-pictures/${username}.jpg`), data, 'base64', (err) => {
-      if (err) {
-        reject();
+    // Strip the data prefix
+    const strippedData = data.replace(/^data:image\/\w+;base64,/, "");
+    fs.writeFile(
+      path.resolve(__dirname, `../public/profile-pictures/${username}.jpg`),
+      strippedData,
+      "base64",
+      err => {
+        if (err) {
+          reject();
+        }
+        resolve();
       }
-      resolve();
-    });
+    );
   });
-  
 };
-
